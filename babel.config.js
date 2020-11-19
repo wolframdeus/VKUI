cbe15a848a3a1e33394e31aec28cab74ccbc24cf
Vladimir Klepov <v.klepov@gmail.com>
2020-11-19 18:02:47 +0300

    TEST: Prevent bundling tests into dist

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,7 @@
+const isProduction = process.env.NODE_ENV === 'production'
 module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
   plugins: ['@babel/plugin-proposal-class-properties', '@babel/plugin-proposal-object-rest-spread', '@babel/plugin-transform-runtime'],
-  ignore: ['./src/vkui.js'],
+  ignore: ['./src/vkui.js'].concat(
+    isProduction ? ['./src/**/*.test.ts', './src/**/*.test.tsx', './src/**/*.spec.ts', './src/**/*.spec.tsx'] : []),
 };
