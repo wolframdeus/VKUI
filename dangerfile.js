d246d4660e69607c403ce13f24a8e3836e5bc473
Vladimir Klepov <v.klepov@gmail.com>
2020-11-25 19:53:13 +0300

    WIP: TEST: Upload failed screens to cloud

--- dangerfile.js
+++ dangerfile.js
@@ -1,13 +1,16 @@
-import { fail, message, warn } from "danger"
+import { fail, message, warn, danger, markdown } from 'danger';
 const dangerJest = require('danger-plugin-jest').default;
-const fs = require('fs');
+const { readFile } = require('fs').promises;
 const path = require('path');
-
-const readFileP = path => new Promise((ok, fail) => fs.readFile(path, (err, file) => err ? fail(err) : ok(file)))
+const glob = require('glob');
+const { promisify } = require('util')
+const pglob = promisify(glob);
+const AWS = require('aws-sdk/global');
+const S3 = require('aws-sdk/clients/s3');
 
 const lintPath = path.join(__dirname, 'lint-results.json');
 function lint() {
-  return readFileP(lintPath).then(file => {
+  return readFile(lintPath).then(file => {
     const lintReport = JSON.parse(file)
     for (const { messages, filePath } of lintReport) {
       const relPath = path.relative(__dirname, filePath)
@@ -24,7 +27,7 @@ function lint() {
 }
 
 function coverage() {
-  return readFileP(path.join(__dirname, 'coverage', 'coverage-summary.json')).then(file => {
+  return readFile(path.join(__dirname, 'coverage', 'coverage-summary.json')).then(file => {
     const { total } = JSON.parse(file)
     const formatCoverage = (kind, { covered, total, pct }) => `${covered} / ${total} ${kind} (${pct}%)`
     message(`Code coverage: ${
@@ -33,8 +36,39 @@ function coverage() {
   })
 }
 
+const UPLOAD_BUCKET = 'vkui-screenshots';
+const awsHost = `${UPLOAD_BUCKET}.hb.bizmrg.com`;
+const s3 = new AWS.S3({
+  apiVersion: '2006-03-01',
+  endpoint: 'hb.bizmrg.com',
+  accessKeyId: 'qDoGa6Z8FFV46kWEPKZPv7',
+  secretAccessKey: '6YuVC54JV3KizQU9RyAyyGGs5vpygktZnfczdk3eEpFG',
+});
+async function uploadFailedScreenshots() {
+  const diffDir = '__diff_output__'
+  const { github } = danger;
+  const pathPrefix = github ? github.pr.number : 'local';
+  for (const failedScreen of await pglob(path.join(__dirname, '**', diffDir, '*.png'))) {
+    const screenName = path.parse(failedScreen).base;
+    const key = [pathPrefix, screenName].join('/');
+    try {
+      await s3.putObject({
+        Body: await readFile(failedScreen),
+        Bucket: UPLOAD_BUCKET,
+        Key: key,
+        ContentType: 'image/png',
+        ACL: 'public-read',
+      }).promise();
+      markdown(`Screenshot \`${screenName}\` failed\n![](https://${awsHost}/${key})`);
+    } catch (err) {
+      console.log('Screenshot diff upload failed', err.message);
+    }
+  }
+}
+
 Promise.all([
   dangerJest(),
   lint(),
-  coverage()
+  coverage(),
+  uploadFailedScreenshots(),
 ]);
