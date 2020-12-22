423ab6684188cb98a6d376e3c3c8819f21210052
Vladimir Klepov <v.klepov@gmail.com>
2020-12-22 15:26:45 +0300

    vkcom/vkui#466: Allow multiple icons in Header aside

--- src/components/Header/Header.e2e.tsx
+++ src/components/Header/Header.e2e.tsx
@@ -18,6 +18,6 @@ describe('Header', () => {
   }, {
     subtitle: ['Поздаголовок'],
   }, {
-    aside: ['Текст', <Link key="">Ссылка</Link>, <Icon24Add key="" />],
+    aside: ['Текст', <Link key="">Ссылка</Link>, <Icon24Add key="" />, <><Icon24Add key="" /><Icon24Add key="" /></>],
   }]);
 });
